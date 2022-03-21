ae9508c8f4ec984badb52379eb7b766800dfc626
CringeMeister <84626613+Anuiel@users.noreply.github.com>
2022-03-21 19:21:07 +0300

    Create Encoder.h

---
+++ Archiver/Encoder/Encoder.h
@@ -0,0 +1,30 @@
+#include "Input/BitReader.h"
+#include "Output/BitWriter.h"
+#include "Archiver/Haffman.h"
+
+namespace Haffman {
+
+class Encoder {
+public:
+    Encoder(const FileName &file_name);
+
+    void AddFile(const FileName &file_name, bool is_file_the_last = false);
+
+private:
+    std::unordered_map<Letter, size_t> GetSymbolFrequency(BitReader &reader);
+
+    std::vector<std::pair<Letter, size_t>> GetNewSymbolLength(std::unordered_map<Letter, size_t> &frequency);
+
+    std::unordered_map<Letter, HaffmanCode> GetNewSymbolsCode(std::vector<std::pair<Letter, size_t>> &length);
+
+    void AddOne(HaffmanCode &code) const;
+
+    void ShiftToLeft(HaffmanCode &code) const;
+
+    size_t LetterToInt(Letter letter);
+
+private:
+    BitWriter out_stream_;
+};
+
+}  // namespace Haffman
